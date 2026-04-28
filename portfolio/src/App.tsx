eec3e86392bd7076b7367751e92ad26001774e54
omann-dev <oliver.mann007@gmail.com>
2026-04-28 16:08:02 +0200

    Add basic nav bar section

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,8 +1,24 @@
+import Header from "./components/Header"
+
 function App() {
   return (
-    <main>
-      <h1>Portfolio</h1>
-    </main>
+    <>
+      <Header />
+
+      <main>
+        <section id="about">
+          <h2>About</h2>
+        </section>
+
+        <section id="projects">
+          <h2>Projects</h2>
+        </section>
+
+        <section id="skills">
+          <h2>Contact</h2>
+        </section>
+      </main>  
+    </>
   );
 }
 
---
+++ portfolio/src/components/Header.tsx
@@ -0,0 +1,20 @@
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <h1>Oliver Mann</h1>
+
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+
+        </header>
+    );
+}
+
+export default Header;
